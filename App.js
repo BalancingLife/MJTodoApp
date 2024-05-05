f8f8ba4c26dd0fc73e81a963607cbc380e83c5bd
김민준 <alswns8495@naver.com>
2024-05-05 18:20:11 +0900

    saveToDos 까지

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,21 +6,85 @@ import {
   StatusBar,
   TouchableOpacity,
   TouchableHighlight,
+  TouchableWithoutFeedback,
+  TextInput,
+  ScrollView,
 } from "react-native";
+import AsnyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./color.js";
 
+const STORAGE_KEY = "@toDos";
+
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
+  const saveToDos = async (toSave) => {
+    //toDos를 string으로, + awiat AsyncStorage.setItem해줄거임
+
+    await AsnyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+
+  const addTodo = async () => {
+    if (text === "") {
+      return;
+    }
+
+    const newTodos = {
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
+
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity activeOpacity={0}>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableHighlight>
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
+
+      <TextInput
+        onSubmitEditing={addTodo}
+        onChangeText={onChangeText}
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "Add a To do" : "Where do you want to go?"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null,
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -39,6 +103,26 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+  },
+  toDoText: {
     color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
